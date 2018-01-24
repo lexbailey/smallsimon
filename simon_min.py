7f561623e04ad9aa4158ce9dadc143ad8b36cacd
Daniel Bailey <danieljabailey@gmail.com>
2018-01-24 21:13:46 +0000

    default argument no longer shorter

--- simon_min.py
+++ simon_min.py
@@ -1,7 +1,7 @@
 from time import*
 s=''
-def e(u=0):print(u or"\x1B[H\x1B[J");sleep(1)
+def e(u):print(u or"\x1B[H\x1B[J");sleep(1)
 while 1:
-	s+='rgby'[int(time())%4];e()
+	s+='rgby'[int(time())%4];e(0)
 	for c in s:e(e(c))
 	for c in s:c!=input()<quit()
