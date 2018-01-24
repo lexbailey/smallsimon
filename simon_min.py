32680c610a125eb1f978433c2df90a72455d3e3e
Daniel Bailey <danieljabailey@gmail.com>
2018-01-24 18:26:20 +0000

    forget multiply by 9, don't really need it, it's random enough

--- simon_min.py
+++ simon_min.py
@@ -2,6 +2,6 @@ from time import*
 s=''
 def e(u=0):print(u or"\x1B[H\x1B[J");sleep(1)
 while 1:
-	s+='rgby'[int(time()*9)%4];e()
+	s+='rgby'[int(time())%4];e()
 	for c in s:e(c);e()
 	for c in s:c==input()or quit()
