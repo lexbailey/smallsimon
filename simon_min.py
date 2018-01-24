b591169679c9aa90c163e591bea44462a1c0c8ef
Daniel Bailey <danieljabailey@gmail.com>
2018-01-24 17:55:32 +0000

    string instead of list

--- simon_min.py
+++ simon_min.py
@@ -1,7 +1,7 @@
 from time import*
-l=sleep;s=[]
+l=sleep;s=''
 def e():print("\x1B[H\x1B[J")
 while 1:
-	s+=["rgby"[int(time()*99)%4]];e()
+	s+="rgby"[int(time()*9)%4];e()
 	for c in s:print(c);l(1);e();l(1)
 	for c in s:c==input()or quit()
