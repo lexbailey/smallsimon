6b283f6b0def7036b5caa0740691bb68f83ee715
Daniel Bailey <danieljabailey@gmail.com>
2018-01-24 21:20:25 +0000

    switch to spaces

--- simon_min.py
+++ simon_min.py
@@ -2,6 +2,6 @@ from time import*
 s=''
 def e(u):print(u or"\x1B[H\x1B[J");sleep(1)
 while 1:
-	s+='rgby'[int(time())%4];e(0)
-	for c in s:e(e(c))
-	for c in s:c!=input()<quit()
+ s+='rgby'[int(time())%4];e(0)
+ for c in s:e(e(c))
+ for c in s:c!=input()<quit()
